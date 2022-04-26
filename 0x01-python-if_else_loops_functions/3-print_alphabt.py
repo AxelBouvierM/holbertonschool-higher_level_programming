23eb56584b84611b6f762b021fafa8a01b1a31bc
AxelBouvierM <axelbouvierm@gmail.com>
2022-04-26 15:16:02 +0000

    program that prints the ASCII alphabet, in lowercase, not followed by a new line with out 'e' and 'q'

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for a in range (97, 123):
+for a in range(97, 123):
     if chr(a) == 'e' or chr(a) == 'q':
         continue
-    print(chr(a), end="")
-
+    print("{}".format(chr(a)), end="")
