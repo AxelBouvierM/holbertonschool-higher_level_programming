be89adabd77e8df60eb73334cc4d0b1a7635138b
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-17 16:56:48 +0000

    defines a square and Instantiation with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """
+    Define a Square class
+    Attributes:
+        None
+    """
+    def __init__(self, size):
+        """
+        Initialization the attributes of Square
+        Args:
+            size (int): The size of a square
+
+        """
+        self.__size = size
