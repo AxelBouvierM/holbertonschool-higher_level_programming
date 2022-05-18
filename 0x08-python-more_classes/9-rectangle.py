cd81fb92248e1fef8ceb2c4d7bb90224fdabf135
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-18 21:43:08 +0000

    Add a class method that returns a new Rectangle instance

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
             width (int): Is the width of a Rectangle
             height (int): Is the height of a Rectangle
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
