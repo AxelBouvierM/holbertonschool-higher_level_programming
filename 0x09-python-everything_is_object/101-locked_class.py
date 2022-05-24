e7cdcb16377609933d6fedefc79c3e58c268744c
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-24 15:16:29 +0000

    with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Define a class by: LockedClass
+"""
+
+
+class LockedClass():
+    """
+    Define a LockedClass
+    Atributes:
+        None
+    """
+    __slots__ = "first_name"
