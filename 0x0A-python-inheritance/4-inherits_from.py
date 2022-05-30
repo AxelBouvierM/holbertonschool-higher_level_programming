c93322a2f8912d023bbb295541b62754b84a186c
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-30 21:12:29 +0000

    fixing task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+returns True if the object is an instance of a class that inherited
+"""
+
+
 def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that inherited
+    Args:
+        obj: Is a object
+        a_class: Is a type
+    """
     if issubclass(type(obj), a_class) is True and type(obj) != a_class:
         return True
     return False
