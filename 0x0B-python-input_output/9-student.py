b00ed98bfd25d8ea7645c5f0a89c5d7f03f35132
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-31 15:25:24 +0000

    class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module create a Student class
+"""
+
+
+class Student():
+    """
+    class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student
+        """
+        return self.__dict__
