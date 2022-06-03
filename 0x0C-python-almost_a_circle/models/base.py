98f5cfd1f9eb685b6df0f47337f1101d2eef75b0
AxelBouvierM <axelbouvierm@gmail.com>
2022-06-03 20:05:24 +0000

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -54,3 +54,10 @@ class Base():
         if json_string is None or len(json_string) == []:
             return "[]"
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+        """
+
