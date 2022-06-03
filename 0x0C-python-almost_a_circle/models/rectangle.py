7d7eac7717aba052cda18143e1394c7f6207a0e9
AxelBouvierM <axelbouvierm@gmail.com>
2022-06-03 14:51:08 +0000

    refresh

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -126,14 +126,27 @@ class Rectangle(Base):
         """
         assigns an argument to each attribute
         """
-        for i in range(len(args)):
-            if i == 0:
-                self.id = args[0]
-            if i == 1:
-                self.width = args[1]
-            if i == 2:
-                self.height = args[2]
-            if i == 3:
-                self.x = args[3]
-            if i == 4:
-                self.y = args[4]
+        if args == None or len(args) == 0:
+            for key, value in kwargs.items():
+                if key == "height":
+                    self.height = value
+                if key == "width":
+                    self.width = value
+                if key == "id":
+                    self.id = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+        else:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
