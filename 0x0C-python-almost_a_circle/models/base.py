26cf97e88a4164cec8a2ac9c78da3adaa9d85709
AxelBouvierM <axelbouvierm@gmail.com>
2022-06-03 17:23:32 +0000

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This module create a Base class
 """
+import json
 
 
 class Base():
@@ -14,10 +15,18 @@ class Base():
         """
         Instanitation of Base class
         Args:
-            id: is a identificator 
+            id: is a identificator
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
