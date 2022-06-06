c469dacbcc3cf1c52c96ed362bd69a63ef8387a5
AxelBouvierM <axelbouvierm@gmail.com>
2022-06-06 18:15:36 +0000

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,60 +1,97 @@
-#!/usr/bin/python3
 """
-Test for Base class
+Unittest for the Base class
 """
+
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-class TestBase(unittest.TestCase):
-    """
-    Test for Base class
-    """
 
-    def test_init(self):
+class TestBase(unittest.TestCase):
+
+    def test_ids(self):
+        """
+        Test id
+        """
+
+        Base._Base__nb_objects = 0
         b1 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 2)
 
-        b2 = Base(2)
-        self.assertEqual(b2.id, 2)
-
-        b3 = Base()
-        self.assertEqual(b3.id, 2)
-
-        b4 = Base()
-        self.assertEqual(b4.id, 3)
-
-    def test_failure(self):
-        with self.assertRaises(TypeError):
-            b5 = Base(2, 3)
-
-        with self.assertRaises(ValueError):
-            b6 = Base(-1)
-
-        with self.assertRaises(ValueError):
-            b7 = Base(0)
-
-    def test_from_json_string(self):
-        list_input = [
-            {'id': 89, 'size': 10},
-            {'id': 7, 'size': 1}
-        ]
-        json_list_input = Base.to_json_string(list_input)
-        list_output = Base.from_json_string(json_list_input)
-        self.assertEqual(list_input, list_output)
-
-        list_input = [
-            {'id': 89, 'width': 10, 'height': 4},
-            {'id': 7, 'width': 1, 'height': 7}
-        ]
-        json_list_input = Base.to_json_string(list_input)
-        list_output = Base.from_json_string(json_list_input)
-        self.assertEqual(list_input, list_output)
-        self.assertEqual(Base.from_json_string("[]"), [])
 
     def test_to_json_string(self):
+        """
+        test to_json_string method
+        """
+
+        Base._Base__nb_objects = 0
+        obj = Base(33)
+        dic = obj.__dict__
 
         self.assertEqual(Base.to_json_string(None), "[]")
         self.assertEqual(Base.to_json_string([]), "[]")
-        self.assertEqual(Base.to_json_string([{'id': 12}]), "[{\"id\": 12}]")
+        self.assertEqual(Base.to_json_string(dic), '{"id": 33}')
+
+    def test_save_to_file(self):
+        """
+        test save_to_file method
+        """
+        Base._Base__nb_objects = 0
+        obj = Base(33)
+
+        Base._Base__nb_objects = 0
+        rec = Rectangle(1, 2, 3, 4, 5)
+        Rectangle.save_to_file([rec])
+        with open("Rectangle.json", "r") as f:
+             read = f.read()
+             new_list = Base.from_json_string(read)
+             self.assertEqual(new_list, [{'id': 5, 'width': 1, 'height': 2, 'x': 3, 'y': 4}])
+
+        Base._Base__nb_objects = 0
+        sq = Square(1, 2, 3, 4)
+        Square.save_to_file([sq])
+        with open("Square.json", "r") as f:
+             read = f.read()
+             new_list = Base.from_json_string(read)
+             self.assertEqual(new_list, [{'id': 4, 'size': 1, 'x': 2, 'y': 3}])
+
+    def test_from_json_string(self):
+        """
+        test from_json_string method
+        """
+
+        Base._Base__nb_objects = 0
+        obj = Base(33)
+        string = [{'id': 33}]
+
+        self.assertEqual(string, [{'id': 33}])
+
+    def test_load_form_file(self):
+        """
+        test load_from_file method
+        """
+
+        Base._Base__nb_objects = 0
+        obj = Base(33)
+
+    def test_create(self):
+        """
+        test create method
+        """
+        
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec_dict = rec.to_dictionary()
+        rec_new = Rectangle.create(**rec_dict)
+        self.assertEqual("[Rectangle] (5) 3/4 - 1/2", str(rec))
+        self.assertEqual("[Rectangle] (5) 3/4 - 1/2", str(rec_new))
+
+        sq = Square(1, 2, 3, 4)
+        sq_dict = sq.to_dictionary()
+        sq_new = Square.create(**sq_dict)
+        self.assertEqual("[Square] (4) 2/3 - 1", str(sq))
+        self.assertEqual("[Square] (4) 2/3 - 1", str(sq_new)) 
