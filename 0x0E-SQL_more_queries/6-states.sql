91a08b4121337857fd05e471c4cc1753689110b9
AxelBouvierM <axelbouvierm@gmail.com>
2022-07-06 21:29:13 +0000

    script that creates the database hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- states description: id INT unique, auto generated, can’t be null and is a primary key name VARCHAR(256) can’t be null
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE, 
 name VARCHAR(256) NOT NULL);
