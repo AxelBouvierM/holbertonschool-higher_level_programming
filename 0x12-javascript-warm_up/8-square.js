1cd14cae7077a17e0b83dc20882d77394162c029
AxelBouvierM <axelbouvierm@gmail.com>
2022-08-01 20:55:09 +0000

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 /// script that prints a square
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    let i = 0;
-    for (i = 0; i < parseInt(process.argv[2]); i++) {
-        let str = ''
-        let x = 0;
-        for (x = 0; x < parseInt(process.argv[2]); x++) {
-            str += 'X';
-        }
-        console.log(str);
+  let i = 0;
+  for (i = 0; i < parseInt(process.argv[2]); i++) {
+    let str = '';
+    let x = 0;
+    for (x = 0; x < parseInt(process.argv[2]); x++) {
+      str += 'X';
     }
+    console.log(str);
+  }
 }
