7abd5751b2375711b78497ec9edfec5fd6067162
AxelBouvierM <axelbouvierm@gmail.com>
2022-08-01 18:44:28 +0000

    Script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,6 @@
 const first = 'C is fun';
 const second = 'Python is cool';
 const third = 'JavaScript is amazing';
-console.log('', first, '\n', second, '\n', third);
+console.log(first);
+console.log(second);
+console.log(third);
