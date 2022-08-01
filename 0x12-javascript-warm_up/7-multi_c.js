b36a55fd7048ba707b702e55dad56049715c5a7c
AxelBouvierM <axelbouvierm@gmail.com>
2022-08-01 20:21:24 +0000

    script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/// script that prints x times “C is fun”
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+    let i = 0;
+    for (i = 0; parseInt(process.argv[2]) > i; i++) {
+        console.log('C is fun');
+    }
+}
