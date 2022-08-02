47a43920ebc2a11ee400fe349b594ac2577d8f19
AxelBouvierM <axelbouvierm@gmail.com>
2022-08-02 16:15:19 +0000

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      let str = '';
+      let x = 0;
+      for (x = 0; x < this.width; x++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const wi = this.width;
+    this.width = this.height;
+    this.height = wi;
+  }
+
+  double () {
+    const wi = this.width;
+    const he = this.height;
+    this.width = wi * 2;
+    this.height = he * 2;
+  }
+};
