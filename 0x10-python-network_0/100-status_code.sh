dd9fedfe1b8d819c4ff26a8d1c7cd65ac9200047
AxelBouvierM <axelbouvierm@gmail.com>
2022-08-18 19:03:40 +0000

    task 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | grep -i "HTTP/" | awk '{print $2}'
+curl -so /dev/null --write-out "%{http_code}" "$1"
