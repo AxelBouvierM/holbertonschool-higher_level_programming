7bc535342d1076ad3b6928c00d9ca6951d6b958c
AxelBouvierM <axelbouvierm@gmail.com>
2022-08-19 15:16:15 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# task 3
+""" Write a Python script that fetches https://intranet.hbtn.io/status """
 if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
